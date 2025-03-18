b3350c8b0ff526af3dad4fcecfdc74d1327bbfee
Vitor Gomes <vitor.araujo.derreteman4@gmail.com>
2025-03-18 15:35:41 -0300

    Codigos exemplo para arvore binaria e lista ordenandas

--- Arvores Binarias/binarias.py
+++ Arvores Binarias/binarias.py
@@ -1,1 +1,14 @@
+class treeNode:
+    def __init__(self, valor):
+        self.valor = valor
+        self.esq = None
+        self.dir = None
 
+n1 = treeNode(10)
+n2 = treeNode(15)
+n3 = treeNode(20)
+
+n1.esq = n2
+n1.dir = n3
+
+print(n1.valor, n1.esq.valor, n1.dir.valor)
--- Listas Ordenadas/Lista Ordenada.py
+++ Listas Ordenadas/Lista Ordenada.py
@@ -1,1 +1,23 @@
+class listaordenada:
+    def __init__(self):
+        self.lista = []
 
+    def adicionar(self, valor):
+
+        for i in range(len(self.lista)):
+            if self.lista[i] > valor:
+                self.lista.insert(i, valor)
+                return
+        
+        self.lista.append(valor)
+
+    def printarli(self):
+        print(self.lista)
+
+lista = listaordenada()
+lista.adicionar(6)
+lista.adicionar(2)
+lista.adicionar(7)
+lista.adicionar(14)
+
+lista.printarli()
