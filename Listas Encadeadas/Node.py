dbeb4bc72596a8bb2cfebd060d9603d3020d93c9
Vitor Gomes <vitor.araujo.derreteman4@gmail.com>
2025-03-18 14:39:42 -0300

    Criação do arquivo explicando como fazer uma lista encadeada

--- Listas Encadeadas/Node.py
+++ Listas Encadeadas/Node.py
@@ -1,1 +1,31 @@
+#Criação de um nó simples
+class Node:
+    def __init__(self, item):
+        self.item = item
+        self.prox = None
 
+class lista_encadeada:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, item):
+        newnode = Node(item)
+        if self.head is None:
+            self.head = newnode
+        else:
+            lastnode = self.head
+            while lastnode.prox:
+                lastnode = lastnode.prox
+            lastnode.prox = newnode
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.item, end=" -> ")
+            current_node = current_node.prox
+        print("None")
+
+Lista_encadeada = lista_encadeada()
+Lista_encadeada.append(10)
+Lista_encadeada.append(20)
+Lista_encadeada.append(10)
+Lista_encadeada.print_list()
